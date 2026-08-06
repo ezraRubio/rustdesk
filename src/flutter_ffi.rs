cfa0221479d7f199355977f6014fcbbd157e3445
ezraRubio <ezra@ezrarubio.com>
2026-08-06 16:25:25 +0300

    qf: point to the right function

--- src/flutter_ffi.rs
+++ src/flutter_ffi.rs
@@ -3204,7 +3204,7 @@ pub mod server_side {
 
     pub unsafe extern "system" fn Java_ffi_FFI_setDeviceName(env: JNIEnv, _class: JClass, name: JString) {
         if let Ok(s) = env.get_string(&name) {
-            main_device_name(s.into());
+            super::main_device_name(s.into());
         }
     }
 }
